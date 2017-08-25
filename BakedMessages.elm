905a3661db2bde707281e133a753f8e1d7f01767
Paul Ivanov <pi@berkeley.edu>
2017-08-25 09:17:05 -0400

    format the outgoing messages prettier

--- BakedMessages.elm
+++ BakedMessages.elm
@@ -9,25 +9,76 @@ module BakedMessages exposing
 
 import JMessages
 
-kernel_info_request_msg = """{"header":{"date": "hi", "msg_type":  "kernel_info_request",
-"msg_id":"f7793152-20a1e0729ce836887c79c611"}, "parent_header": {}, "metadata":{}}"""
+kernel_info_request_msg = """
+{
+  "header": {
+    "msg_type": "kernel_info_request",
+    "msg_id": "f7793152-20a1e0729ce836887c79c611"
+  },
+  "parent_header": {},
+  "content": {},
+  "metadata": {}
+}
+"""
 
 empty_execute_request_msg = """{"header":{"msg_type":  "execute_request", "msg_id":""}, "parent_header": {}, "metadata":{}}"""
 
-error_execute_request_msg = """{"header":{"msg_type":  "execute_request",
-"msg_id":"2282972d-858548e9788469565e4d1984"}, "parent_header": {}, "content":
-{"code": "import IPython.displas as d; d.HTML('<b>fancy</b>'"
-,"silent": false , "store_history": true , "user_expressions" : {} , "allow_stdin": true , "stop_on_error": true }, "metadata":{}}"""
+error_execute_request_msg = """
+{
+  "header": {
+    "msg_type": "execute_request",
+    "msg_id": "2282972d-858548e9788469565e4d1984"
+  },
+  "parent_header": {},
+  "content": {
+    "code": "import IPython.displas as d; d.HTML('<b>fancy</b>'",
+    "silent": false,
+    "store_history": true,
+    "user_expressions": {},
+    "allow_stdin": true,
+    "stop_on_error": true
+  },
+  "metadata": {}
+}
+"""
 
-fancy_execute_request_msg = """{"header":{"msg_type":  "execute_request",
-"msg_id":"c81b7217-e75dbf6e838637710e829864"}, "parent_header": {}, "content":
-{"code": "import IPython.display as d; import time; d.display(d.Image('thumb/exitingvim-big.png'));time.sleep(1); d.HTML('<b>fancy</b>')"
-,"silent": false , "store_history": true , "user_expressions" : {} , "allow_stdin": true , "stop_on_error": true }, "metadata":{}}"""
+fancy_execute_request_msg = """
+{
+  "header": {
+    "msg_type": "execute_request",
+    "msg_id": "c81b7217-e75dbf6e838637710e829864"
+  },
+  "parent_header": {},
+  "content": {
+    "code": "import IPython.display as d; import time; d.display(d.Image('thumb/exitingvim-big.png'));time.sleep(1); d.HTML('<b>fancy</b>')",
+    "silent": false,
+    "store_history": true,
+    "user_expressions": {},
+    "allow_stdin": true,
+    "stop_on_error": true
+  },
+  "metadata": {}
+}
+"""
 
-stdout_execute_request_msg = """{"header":{"msg_type":  "execute_request",
-"msg_id":"7d2c0b07-56bbeb2574a05672fbafe450"}, "parent_header": {}, "content":
-{"code": "print('hallo JupyterCon!')"
-,"silent": false , "store_history": true , "user_expressions" : {} , "allow_stdin": true , "stop_on_error": true }, "metadata":{}}"""
+stdout_execute_request_msg = """
+{
+  "header": {
+    "msg_type": "execute_request",
+    "msg_id": "7d2c0b07-56bbeb2574a05672fbafe450"
+  },
+  "parent_header": {},
+  "content": {
+    "code": "print('hallo JupyterCon!')",
+    "silent": false,
+    "store_history": true,
+    "user_expressions": {},
+    "allow_stdin": true,
+    "stop_on_error": true
+  },
+  "metadata": {}
+}
+"""
 
 
 
